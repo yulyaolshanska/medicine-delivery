7d846dc8e35dc1c4e4a678f97e1cd4c00e49590e
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2024-03-06 23:01:19 +0200

    Update ShoppingForm.tsx

--- src/components/shoppingForm/ShoppingForm.tsx
+++ src/components/shoppingForm/ShoppingForm.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { emailRegexp, nameRegexp } from "../../constants/validation";
 import styles from "./ShoppingForm.module.scss";
